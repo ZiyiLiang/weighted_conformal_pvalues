7adae5b1b39d40eca9b7105e83d33d4925d9d090
Matteo Sesia <sesia@marshall.usc.edu>
2022-06-22 18:53:35 -0700

    Written code for CV experiments

---
+++ experiments_synthetic/experiment_cv.py
@@ -0,0 +1,214 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.stats.multitest import multipletests
+from tqdm import tqdm
+import pdb
+
+# Binary classifiers
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, OneClassSVM
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+import os, sys
+sys.path.append("../methods")
+
+from models import GaussianMixture, ConcentricCircles, ConcentricCirclesMixture, BinomialModel
+from methods_split import BinaryConformal, OneClassConformal, IntegrativeConformal
+from methods_cv import BinaryConformal as CV_BinaryConformal
+from methods_cv import OneClassConformal as CV_OneClassConformal
+from methods_cv import IntegrativeConformal as CV_IntegrativeConformal
+
+from util_experiments import eval_pvalues
+
+#########################
+# Experiment parameters #
+#########################
+
+if True: # Input parameters
+    # Parse input arguments
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    print ('Argument List:', str(sys.argv))
+    model_num = 1
+    if len(sys.argv) != 8:
+        print("Error: incorrect number of parameters.")
+        quit()
+
+    setup = int(sys.argv[1])
+    data_name = sys.argv[2]
+    n = int(sys.argv[3])
+    p = int(sys.argv[4])
+    a = float(sys.argv[5])
+    purity = float(sys.argv[6])
+    random_state = int(sys.argv[7])
+
+else: # Default parameters
+    setup = 1
+    data_name = "circles"
+    n = 1000
+    p = 100
+    a = 0.9
+    purity = 0.8
+    random_state = 2022
+
+
+# Fixed experiment parameters
+n_test = 100
+purity_test = 0.5
+n_folds = 10
+calib_size = 0.5
+alpha_list = [0.01, 0.02, 0.05, 0.1, 0.2]
+num_repetitions = 2
+
+# List of possible one-class classifiers with desired hyper-parameters
+oneclass_classifiers = {
+    'SVM-rbf': OneClassSVM(kernel='rbf', degree=3),
+    'SVM-sig': OneClassSVM(kernel='sigmoid', degree=3),
+    'SVM-pol': OneClassSVM(kernel='poly', degree=3),
+    'IF': IsolationForest(random_state=random_state),
+    'LOF': LocalOutlierFactor(novelty=True)
+}
+
+# Define list of possible two-class classifiers with desired hyper-parameters
+binary_classifiers = {
+    'RF': RandomForestClassifier(random_state=random_state),
+    'KNN': KNeighborsClassifier(),
+    'SVC': SVC(probability=True),
+    'NB' : GaussianNB(),
+    'QDA': QuadraticDiscriminantAnalysis(),
+    'MLP': MLPClassifier(max_iter=500, random_state=random_state)
+}
+
+###############
+# Output file #
+###############
+outfile_prefix = "results/setup_cv" + str(setup) + "/" +str(data_name) + "_n"+str(n) + "_p" + str(p) + "_a" + str(a) + "_purity" + str(purity) + "_seed" + str(random_state)
+outfile = outfile_prefix + ".txt"
+print("Output file: {:s}".format(outfile), end="\n")
+
+# Header for results file
+def add_header(df):
+    df["Setup"] = setup
+    df["Data"] = data_name
+    df["n"] = n
+    df["p"] = p
+    df["Signal"] = a
+    df["Purity"] = purity
+    df["Seed"] = random_state    
+    return df
+
+#########################
+# Data-generating model #
+#########################
+
+class DataSet:
+    def __init__(self, data_name, random_state=None):
+        if data_name=="circles":
+            self.model = ConcentricCircles(p, a, random_state=random_state)
+        elif data_name=="circles-mixed":
+            self.model = ConcentricCirclesMixture(p, a, random_state=random_state)
+        elif data_name=="binomial":
+            self.model = BinomialModel(p, a, random_state=random_state)
+        else:
+            print("Error: unknown model name!")
+            exit(0)
+
+    def sample(self, n, purity):
+        return self.model.sample(n, purity)
+
+###################
+# Run experiments #
+###################
+
+def run_experiment(dataset, random_state):
+    # Sample the training/calibration data
+    X, Y = dataset.sample(n, purity)
+    X_in = X[Y==0]
+    X_out = X[Y==1]
+    # Sample the test data
+    X_test, Y_test = dataset.sample(n_test, purity_test)
+
+    # Initialize result data frame
+    results = pd.DataFrame({})
+
+    # Conformal p-values via CV+ binary classification
+    print("Running {:d} binary classifiers...".format(len(binary_classifiers)))
+    sys.stdout.flush()
+    for bc_name in tqdm(binary_classifiers.keys()):
+        bc = binary_classifiers[bc_name]
+        method = CV_BinaryConformal(X_in, X_out, bc, n_folds=n_folds, verbose=False)
+        pvals_test = method.compute_pvalues(X_test)
+        results_tmp = eval_pvalues(pvals_test, Y_test, alpha_list)
+        results_tmp["Method"] = "Binary (CV+)"
+        results_tmp["Model"] = bc_name
+        results = pd.concat([results, results_tmp])
+
+    # Conformal p-values via one-class classification
+    print("Running {:d} one-class classifiers...".format(len(oneclass_classifiers)))
+    sys.stdout.flush()
+    for occ_name in tqdm(oneclass_classifiers.keys()):
+        occ = oneclass_classifiers[occ_name]
+        method = CV_OneClassConformal(X_in, occ, n_folds=n_folds, verbose=False)
+        pvals_test = method.compute_pvalues(X_test)
+        results_tmp = eval_pvalues(pvals_test, Y_test, alpha_list)
+        results_tmp["Method"] = "One-Class (CV+)"
+        results_tmp["Model"] = occ_name
+        results = pd.concat([results, results_tmp])
+
+    ## Conformal p-values via split-integrative
+    print("Running weighted classifiers with learning ensemble...")
+    sys.stdout.flush()
+    bboxes_one = list(oneclass_classifiers.values())
+    bboxes_two = list(binary_classifiers.values())
+    method = IntegrativeConformal(X_in, X_out,
+                                  bboxes_one=bboxes_one, bboxes_two=bboxes_two,
+                                  calib_size=calib_size, tuning=True, progress=True, verbose=False)
+    pvals_test, pvals_test_0, pvals_test_1 = method.compute_pvalues(X_test, return_prepvals=True)
+    results_tmp = eval_pvalues(pvals_test, Y_test, alpha_list)
+    results_tmp["Method"] = "Ensemble (split)"
+    results_tmp["Model"] = "Ensemble"
+    results = pd.concat([results, results_tmp])
+
+    ## Conformal p-values via CV+-integrative
+    print("Running weighted classifiers with learning ensemble...")
+    sys.stdout.flush()
+    bboxes_one = list(oneclass_classifiers.values())
+    bboxes_two = list(binary_classifiers.values())
+    method = CV_IntegrativeConformal(X_in, X_out,
+                                       bboxes_one=bboxes_one, bboxes_two=bboxes_two,
+                                       n_folds=n_folds, tuning=True, progress=True, verbose=False)
+    pvals_test, pvals_test_0, pvals_test_1 = method.compute_pvalues(X_test, return_prepvals=True)
+    results_tmp = eval_pvalues(pvals_test, Y_test, alpha_list)
+    results_tmp["Method"] = "Ensemble (CV+)"
+    results_tmp["Model"] = "Ensemble"
+    results = pd.concat([results, results_tmp])
+
+
+    return results
+
+# Initialize result data frame
+results = pd.DataFrame({})
+
+for r in range(num_repetitions):
+    print("\nStarting repetition {:d} of {:d}:\n".format(r+1, num_repetitions))
+    sys.stdout.flush()
+    # Change random seed for this repetition
+    random_state_new = 10*num_repetitions*random_state + r
+    dataset = DataSet(data_name, random_state=random_state_new)
+    # Run experiment and collect results
+    results_new = run_experiment(dataset, random_state_new)
+    results_new = add_header(results_new)
+    results_new["Repetition"] = r
+    results = pd.concat([results, results_new])
+    # Save results
+    results.to_csv(outfile, index=False)
+    print("\nResults written to {:s}\n".format(outfile))
+    sys.stdout.flush()
+
+print("\nAll experiments completed.\n")
+sys.stdout.flush()
---
+++ experiments_synthetic/submit_experiment_cv.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Parameters
+SETUP="1"
+
+if [[ $SETUP == 1 ]]; then
+  DATA_LIST=("circles-mixed")
+  N_LIST=(20 30 50 100 200 500 1000)
+  P_LIST=(1000)
+  A_LIST=(0.7)
+  PURITY_LIST=(0.5 0.75 0.9)
+  SEED_LIST=$(seq 1 1)
+
+elif [[ $SETUP == 2 ]]; then
+  DATA_LIST=("circles-mixed")
+  N_LIST=(200 2000)
+  P_LIST=(1000)
+  A_LIST=(0.7)
+  PURITY_LIST=(0.5 0.6 0.7 0.8 0.9 0.95 0.98 0.99 1.0)
+  SEED_LIST=$(seq 1 100)
+
+elif [[ $SETUP == 3 ]]; then
+  DATA_LIST=("circles-mixed")
+  N_LIST=(20 50 100 200 500 1000 2000 5000)
+  P_LIST=(1000)
+  A_LIST=(1.25)
+  PURITY_LIST=(0.5 0.75 0.9)
+  SEED_LIST=$(seq 1 100)
+
+elif [[ $SETUP == 4 ]]; then
+  DATA_LIST=("binomial")
+  N_LIST=(20 50 100 200 500 1000 2000 5000)
+  P_LIST=(100)
+  A_LIST=(3)
+  PURITY_LIST=(0.5 0.75 0.9)
+  SEED_LIST=$(seq 1 100)
+
+fi
+
+# Slurm parameters
+MEMO=5G                             # Memory required (5 GB)
+TIME=00-00:20:00                    # Time required (20 m)
+CORE=1                              # Cores required (1)
+
+# Assemble order prefix
+ORDP="sbatch --mem="$MEMO" --nodes=1 --ntasks=1 --cpus-per-task=1 --time="$TIME
+#ORDP="sbatch --account=sesia_658 --partition=sesia,shared --mem="$MEMO" --nodes=1 --ntasks=1 --cpus-per-task=1 --time="$TIME
+
+# Create directory for log files
+LOGS="logs"
+mkdir -p $LOGS
+mkdir -p $LOGS"/setup_cv"$SETUP
+
+OUT_DIR="results"
+mkdir -p $OUT_DIR
+mkdir -p $OUT_DIR"/setup_cv"$SETUP
+
+# Loop over configurations and chromosomes
+for SEED in $SEED_LIST; do
+  for DATA in "${DATA_LIST[@]}"; do
+
+    for N in "${N_LIST[@]}"; do
+      for P in "${P_LIST[@]}"; do
+        for A in "${A_LIST[@]}"; do
+          for PURITY in "${PURITY_LIST[@]}"; do
+
+            JOBN="setup_cv"$SETUP"/"$DATA"_n"$N"_p"$P"_a"$A"_purity"$PURITY"_seed"$SEED
+            OUT_FILE=$OUT_DIR"/"$JOBN".txt"
+            COMPLETE=0
+            #ls $OUT_FILE
+            if [[ -f $OUT_FILE ]]; then
+              COMPLETE=1
+            fi
+
+            if [[ $COMPLETE -eq 0 ]]; then
+              # Script to be run
+              SCRIPT="experiment_cv.sh $SETUP $DATA $N $P $A $PURITY $SEED"
+              # Define job name for this chromosome
+              OUTF=$LOGS"/"$JOBN".out"
+              ERRF=$LOGS"/"$JOBN".err"
+              # Assemble slurm order for this job
+              ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
+              # Print order
+              echo $ORD
+              # Submit order
+              #$ORD
+              # Run command now
+              #./$SCRIPT
+            fi
+          done
+        done
+      done
+    done
+  done
+done
