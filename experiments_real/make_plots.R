8eb38d9ca59981ba3144028b44496440ef4fcc46
Matteo Sesia <sesia@marshall.usc.edu>
2022-07-01 22:30:55 -0700

    Finished experiments with CV

--- experiments_real/make_plots.R
+++ experiments_real/make_plots.R
@@ -5,7 +5,9 @@ library(tidyverse)
 plot.1 <- FALSE  # Flowers
 plot.2 <- FALSE  # Animals
 plot.3 <- FALSE  # Cars
-plot.4 <- TRUE  # Tabular
+plot.4 <- FALSE  # Tabular
+plot.5 <- TRUE  # Flowers-CV
+plot.6 <- FALSE  # Tabular-CV
 
 #############
 ## Setup 1 ##
@@ -650,5 +652,193 @@ if(plot.4) {
         theme(legend.key.width = unit(1,"cm"),
               plot.title = element_text(hjust = -0.11, vjust = -4))
     pp.2 %>% ggsave(file=sprintf("figures/experiment_real_tabular_oracle_bin.pdf", ifelse(plot.fdr, "bh", "fixed")), width=5.5, height=2.25, units="in")
+
+}
+
+
+
+
+if(plot.5) {
+    idir <- "results_cv_hpc/"
+    ifile.list <- list.files(idir)
+
+    results.raw <- do.call("rbind", lapply(ifile.list, function(ifile) {
+        if(startsWith(ifile, "images_flowers")) {
+            df <- read_delim(sprintf("%s/%s", idir, ifile), delim=",", col_types=cols())
+        } else {
+            df <- tibble()
+        }
+    }))
+   
+    method.values <- c("Ensemble (CV)", "Ensemble", "One-Class (CV)", "Binary (CV)")
+    method.labels <- c("Integrative (TCV+)", "Integrative", "One-Class (oracle, CV+)", "Binary (oracle, CV+)")
+    color.scale <- c("#da00da", "darkviolet", "red", "blue", "darkgreen", "green")
+    shape.scale <- c(5, 8, 3, 1, 1)
+    alpha.scale <- c(1, 1, 0.5, 0.5)
+
+    plot.fdr <- TRUE
+
+    if(plot.fdr) {
+        results <- results.raw %>%
+            mutate(TypeI=`Storey-BH-FDP`, Power=`Storey-BH-Power`)
+        metric.values <- c("Power", "TypeI")
+        metric.labels <- c("Power", "FDR")
+    } else {
+        results <- results.raw %>%
+            mutate(TypeI=`Fixed-FPR`, Power=`Fixed-TPR`)
+        metric.values <- c("Power", "TypeI")
+        metric.labels <- c("TPR", "FPR")
+    }
+
+    alpha.nominal <- 0.1
+    df.nominal <- tibble(Metric="TypeI", Mean=alpha.nominal) %>%
+        mutate(Metric = factor(Metric, metric.values, metric.labels))
+
+    results.fdr.models <- results %>%
+        group_by(Data, n_in, n_out, Method, Model, Alpha) %>%
+        summarise(Power.se=2*sd(Power)/sqrt(n()), Power=mean(Power), TypeI.se=2*sd(TypeI)/sqrt(n()), TypeI=mean(TypeI))
+
+    results.fdr.oracle <- results %>%
+        filter(Method %in% c("Binary", "One-Class", "Binary (CV)", "One-Class (CV)")) %>%
+        group_by(Data, n_in, n_out, Method, Model, Alpha) %>%
+        summarise(Power.se=2*sd(Power)/sqrt(n()), Power=mean(Power), TypeI.se=2*sd(TypeI)/sqrt(n()), TypeI=mean(TypeI)) %>%
+        group_by(Data, n_in, n_out, Method, Alpha) %>%
+        summarise(idx.oracle = which.max(Power), Model="Oracle", Power=Power[idx.oracle], Power.se=Power.se[idx.oracle],
+                  TypeI=TypeI[idx.oracle], TypeI.se=TypeI.se[idx.oracle]) %>%
+        select(-idx.oracle)
+
+    df <- results.fdr.models %>%
+        filter(Method %in% c("Ensemble", "Ensemble (CV)")) %>%
+        rbind(results.fdr.oracle)
+    results.fdr.mean <- df %>%
+        gather(Power, TypeI, key="Metric", value="Mean") %>%
+        select(-Power.se, -TypeI.se)
+    results.fdr.se <- df %>%
+        gather(Power.se, TypeI.se, key="Metric", value="SE") %>%
+        select(-Power, -TypeI) %>%
+        mutate(Metric = ifelse(Metric=="Power.se", "Power", Metric),
+               Metric = ifelse(Metric=="TypeI.se", "TypeI", Metric))
+    results.fdr <- results.fdr.mean %>% inner_join(results.fdr.se) %>%
+        mutate(Metric = factor(Metric, metric.values, metric.labels))
+
+    pp <- results.fdr %>%
+#        filter(Metric %in% c("Power","TPR")) %>%
+        filter(n_in %in% c(513)) %>%
+        filter(Alpha==alpha.nominal) %>%
+        filter(Method %in% method.values) %>%
+        mutate(Method = factor(Method, method.values, method.labels)) %>%
+#        mutate(Data = factor(Data, data.values, data.labels)) %>%
+        ggplot(aes(x=n_out, y=Mean, color=Method, shape=Method, alpha=Method)) +
+        geom_point() +
+        geom_line() +
+        geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=0.1) +
+#        geom_hline(aes(yintercept=Mean), data=df.nominal, linetype=2) +
+        facet_wrap(.~Metric) +
+        scale_x_log10() +
+        scale_y_continuous(lim=c(0,1)) +
+        scale_color_manual(values=color.scale) +
+        scale_shape_manual(values=shape.scale) +
+        scale_alpha_manual(values=alpha.scale) +
+        xlab("Number of outliers") +
+        ylab("") +
+        theme_bw()
+    pp %>% ggsave(file=sprintf("figures/experiment_real_cv_flowers.pdf", ifelse(plot.fdr, "bh", "fixed")), width=6, height=2.25, units="in")
+
+
+
+}
+
+
+
+if(plot.6) {
+    idir <- "results_cv_hpc/"
+    ifile.list <- list.files(idir)
+
+    results.raw <- do.call("rbind", lapply(ifile.list, function(ifile) {
+        if(startsWith(ifile, "annthyroid") | startsWith(ifile, "mammography")) {
+            df <- read_delim(sprintf("%s/%s", idir, ifile), delim=",", col_types=cols())
+        } else {
+            df <- tibble()
+        }
+    }))
+
+    data.values <- c("annthyroid", "mammography")
+    data.labels <- c("Thyroid disease", "Mammography")
+    
+    method.values <- c("Ensemble (CV)", "One-Class (CV)", "Binary (CV)", "Ensemble", "One-Class", "Binary")
+    method.labels <- c("Integrative (CV)", "OCC (CV, oracle)", "Binary (CV, oracle)", "Integrative", "OCC (oracle)", "Binary (oracle)")
+    color.scale <- c("darkviolet", "red", "blue", "darkviolet", "red", "blue")
+    shape.scale <- c(8, 3, 1, 8, 3, 1)
+    alpha.scale <- c(1, 0.75, 0.75, 0.5, 0.25, 0.25)
+
+    plot.fdr <- TRUE
+
+    if(plot.fdr) {
+        results <- results.raw %>%
+            mutate(TypeI=`Storey-BH-FDP`, Power=`Storey-BH-Power`)
+        metric.values <- c("Power", "TypeI")
+        metric.labels <- c("Power", "FDR")
+    } else {
+        results <- results.raw %>%
+            mutate(TypeI=`Fixed-FPR`, Power=`Fixed-TPR`)
+        metric.values <- c("Power", "TypeI")
+        metric.labels <- c("TPR", "FPR")
+    }
+
+    alpha.nominal <- 0.1
+    df.nominal <- tibble(Metric="TypeI", Mean=alpha.nominal) %>%
+        mutate(Metric = factor(Metric, metric.values, metric.labels))
+
+    results.fdr.models <- results %>%
+        group_by(Data, n_in, n_out, Method, Model, Alpha) %>%
+        summarise(Power.se=2*sd(Power)/sqrt(n()), Power=mean(Power), TypeI.se=2*sd(TypeI)/sqrt(n()), TypeI=mean(TypeI))
+
+    results.fdr.oracle <- results %>%
+        filter(Method %in% c("Binary", "One-Class", "Binary (CV)", "One-Class (CV)")) %>%
+        group_by(Data, n_in, n_out, Method, Model, Alpha) %>%
+        summarise(Power.se=2*sd(Power)/sqrt(n()), Power=mean(Power), TypeI.se=2*sd(TypeI)/sqrt(n()), TypeI=mean(TypeI)) %>%
+        group_by(Data, n_in, n_out, Method, Alpha) %>%
+        summarise(idx.oracle = which.max(Power), Model="Oracle", Power=Power[idx.oracle], Power.se=Power.se[idx.oracle],
+                  TypeI=TypeI[idx.oracle], TypeI.se=TypeI.se[idx.oracle]) %>%
+        select(-idx.oracle)
+
+    df <- results.fdr.models %>%
+        filter(Method %in% c("Ensemble", "Ensemble (CV)")) %>%
+        rbind(results.fdr.oracle)
+    results.fdr.mean <- df %>%
+        gather(Power, TypeI, key="Metric", value="Mean") %>%
+        select(-Power.se, -TypeI.se)
+    results.fdr.se <- df %>%
+        gather(Power.se, TypeI.se, key="Metric", value="SE") %>%
+        select(-Power, -TypeI) %>%
+        mutate(Metric = ifelse(Metric=="Power.se", "Power", Metric),
+               Metric = ifelse(Metric=="TypeI.se", "TypeI", Metric))
+    results.fdr <- results.fdr.mean %>% inner_join(results.fdr.se) %>%
+        mutate(Metric = factor(Metric, metric.values, metric.labels))
+
+    pp <- results.fdr %>%
+#        filter(Metric %in% c("Power","TPR")) %>%
+        filter(n_in %in% c(6399, 10000)) %>%
+        filter(Alpha==alpha.nominal) %>%
+        filter(Method %in% method.values) %>%
+        mutate(Method = factor(Method, method.values, method.labels)) %>%
+        mutate(Data = factor(Data, data.values, data.labels)) %>%
+        ggplot(aes(x=n_out, y=Mean, color=Method, shape=Method, alpha=Method)) +
+        geom_point() +
+        geom_line() +
+#        geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=0.1) +
+#        geom_hline(aes(yintercept=Mean), data=df.nominal, linetype=2) +
+        facet_wrap(Metric~Data) +
+        scale_x_log10() +
+        scale_y_continuous(lim=c(0,1)) +
+        scale_color_manual(values=color.scale) +
+        scale_shape_manual(values=shape.scale) +
+        scale_alpha_manual(values=alpha.scale) +
+        xlab("Number of outliers") +
+        ylab("") +
+        theme_bw()
+    pp %>% ggsave(file=sprintf("figures/experiment_real_cv_tabular.pdf", ifelse(plot.fdr, "bh", "fixed")), width=5.5, height=4, units="in")
+
+
 
 }
